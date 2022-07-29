c7ebf4f99a89fed2f581d9d1df1e5b72dc9dc108
Alexander <aleka_alexander@mail.ru>
2022-07-29 15:01:19 +0300

    add scene and refactor

---
+++ src/Scene.py
@@ -0,0 +1,39 @@
+from Figure import Figure
+from typing import List, Optional
+
+from matplotlib import pyplot as plt
+
+class Scene:
+    def __init__(self, figures: Optional[List[Figure]] = None, workspace: float = 100):
+        if figures is None:
+            self.__figures = []
+        else:
+            assert isinstance(figures, list) and workspace > 0
+            self.__figures = figures
+        self.__workspace = workspace
+
+    def add(self, figure: Figure):
+        assert isinstance(figure, Figure)
+        self.__figures.append(figure)
+
+    def figures(self) -> List[Figure]:
+        return self.__figures.copy()
+
+    def figures_count(self) -> int:
+        return len(self.__figures)
+
+    def mutable_params_count(self) -> int:
+        n = 0
+        for i in self.__figures:
+            n += len(i.mu_params)
+        return n
+
+    def __is_figure_in_workspace(self, figure: Figure):
+        raise NotImplementedError("")
+
+    def plot(self, n: int = 10000, scatter: bool = False):
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        for i in self.__figures:
+            i.plot(ax, n, scatter=scatter)
+        plt.show()
--- src/main.py
+++ src/main.py
@@ -4,10 +4,14 @@ from tqdm import tqdm
 from copy import copy
 from typing import List, Tuple, Union
 
+import pygad
+from itertools import combinations
+
 from Parlgram import Parlgram
 from Cylinder import Cylinder
 from Sphere import Sphere
 from Figure import Figure
+from Scene import Scene
 
 from matplotlib import pyplot as plt
 
@@ -30,41 +34,97 @@ def isIntersect(cyl: Cylinder, parl: Parlgram):
 
     return sol.x
 
-
-def isIntersect_pro(cyl: Figure, parl: Figure) -> bool:
-    cons = parl.ineq() + cyl.ineq()
-
+def isIntersect_pro_pair(figure_1: Figure, figure_2: Figure) -> bool:
+    cons = figure_1.ineq() + figure_2.ineq()
     f = lambda x: 0
     # method= COBYLA, SLSQP
     sol = optimize.minimize(f, x0=np.random.uniform(-1, 1, 3), constraints=cons, method="COBYLA")
     return sol.success
 
-def F(x: List[float], *args: Figure) -> float:
-    const_parlgram, const_cylinder = args
+def isIntersect_pro(figures: List[Figure]) -> bool:
+    combs = combinations(figures, 2)
+    for i in combs:
+        if isIntersect_pro_pair(i[0], i[1]):
+            return True
+    return False
 
-    new_cyl = const_cylinder.add_tol_to_mu_params(x[:len(const_cylinder.mu_params)])
-    new_parl = const_parlgram.add_tol_to_mu_params(x[len(const_cylinder.mu_params):])
+def split(x: List[float], figures: List[Figure]) -> List[List[float]]:
+    res = []
+    prev_n = 0
+    for i in figures:
+        n = len(i.mu_params)
+        res.append(x[prev_n: prev_n + n])
+        prev_n = n
+    return res
 
-    if isIntersect_pro(cyl=new_cyl, parl=new_parl):
-        return 666
-    return -min(x)*100 - sum(x)  # better to use "-min(x)"
+def add_tol_to_figures(tols: List[float], figures: List[Figure]):
+    tols = split(tols, figures)
+    new_figures = []
+    for i in range(len(figures)):
+        new_figures.append(figures[i].add_tol_to_mu_params(tols[i]))
+    return new_figures
 
+def F(x: List[float], *args: Scene) -> float:
+    figures: List[Figure] = args[0].figures()
+
+    new_figures = add_tol_to_figures(x, figures)
+
+    if isIntersect_pro(new_figures):
+        return np.random.uniform(666, 999)
+    return -min(x)  # better to use "-min(x)"
+
+def differential_evolution(scene: Scene):
+    bounds = [(0, 10) for _ in range(scene.mutable_params_count())]
+    sol = optimize.differential_evolution(F, args=(scene,), bounds=bounds, maxiter=100, disp=True, workers=8)
+    return sol.x
+
+def genetic_algorithm(scene: Scene):
+    def fitness_func(solution, solution_idx):
+        figures: List[Figure] = scene.figures()
+
+        new_figures = add_tol_to_figures(solution, figures)
+
+        if isIntersect_pro(new_figures):
+            return -np.random.uniform(666, 999)
+        return min(solution)  # better to use "-min(x)"
+
+    def callback_on_generation(ga_instance):
+        print("Generation:", ga_instance.generations_completed)
+        print("Fitness of the best solution :", fitness_func(ga_instance.best_solutions[-1], 0))
+
+    gene_space = [{'low': 0, 'high': 10} for _ in range(scene.mutable_params_count())]
+    ga_instance = pygad.GA(num_generations=100,
+                           num_parents_mating=5,
+                           fitness_func=fitness_func,
+                           num_genes=len(gene_space),
+                           sol_per_pop=100,
+                           gene_space=gene_space,
+                           save_best_solutions=True,
+                           on_generation=callback_on_generation
+                           )
+    ga_instance.run()
+    solution, solution_fitness, solution_idx = ga_instance.best_solution()
+    print("Parameters of the best solution : {solution}".format(solution=solution))
+    print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+    return solution
+
+def main():
+    g_parlgram = Parlgram([2, 2, 2], [2, 2, 2])
+    g_parlgram2 = Parlgram([-5, -5, -5], [0.25, 0.25, 0.25])
+    g_cylinder = Cylinder([0, 0, 0], 2, 1)
+    g_sphere = Sphere([-15, -15, -15], 0.25)
+
+    scene = Scene([g_parlgram, g_cylinder, g_parlgram2, g_sphere])
+
+    solution = differential_evolution(scene)
+
+    new_figs = add_tol_to_figures(solution, scene.figures())
+    new_scene = Scene(new_figs)
+
+    scene.plot()
+    new_scene.plot()
+
+    print("Пересекаются ли фигуры:", isIntersect_pro(new_scene.figures()))
 
 if __name__ == '__main__':
-    g_parlgram = Parlgram([2, 2, 2], [2, 2, 2])
-    g_cylinder = Cylinder([0, 0, 0], 2, 1)
-    g_sphere = Sphere([-5, -5, -5], 0.25)
-
-    # fig = plt.figure()
-    # ax = fig.add_subplot(projection='3d')
-    # g_parlgram.plot(ax, 10000, scatter=True)
-    # g_cylinder.plot(ax, 10000, scatter=True)
-    # g_sphere.plot(ax, 1000, scatter=True)
-    # plt.show()
-
-    bounds = [(0, 1) for _ in range(5)]
-    sol = optimize.differential_evolution(F, args=(g_parlgram, g_cylinder), bounds=bounds, maxiter=100, disp=True, workers=8)
-    print(sol)
-
-
-    print(isIntersect_pro(g_cylinder, g_parlgram))
+    main()
