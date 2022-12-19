3dbb7e340203f75807770765bf0087aeab5abcf1
Alexander <aleka_alexander@mail.ru>
2022-12-19 19:05:56 +0300

    bug fix
    bug fix

--- src/utils.py
+++ src/utils.py
@@ -8,11 +8,11 @@ def split(x: List[float], figures: List[Figure]) -> List[List[float]]:
     for i in figures:
         n = len(i.mu_params())
         res.append(x[prev_n: prev_n + n])
-        prev_n = n
+        prev_n += n
     return res
 
 def distance(x: List[float], y: List[float]) -> float:
-    return ((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2 + (x[2] - y[2])) ** 0.5
+    return ((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2 + (x[2] - y[2]) ** 2) ** 0.5
 
 def add_tol_to_figures(tols: List[float], figures: List[Figure]):
     tols = split(tols, figures)
