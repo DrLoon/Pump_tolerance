bafd3ff83ff7b14ed6cb991873d8af99e270fccb
Alexander <aleka_alexander@mail.ru>
2022-12-20 12:38:00 +0300

    add upper_cut cone and angle fix

--- src/Figures/Cone.py
+++ src/Figures/Cone.py
@@ -5,14 +5,16 @@ from .Figure import Figure
 
 
 class Cone(Figure):
-    def __init__(self, center: List[float], angle: float, height: float):
+    def __init__(self, center: List[float], angle: float, height: float, upper_cut: float = 0):
         assert len(center) == 3
-        assert 0 < angle < np.pi / 2 and height > 0
+        assert 0 < angle <= np.pi / 2 and height > 0
+        assert 0 <= upper_cut < height
         self.x0 = center[0]
         self.y0 = center[1]
         self.z0 = center[2]
         self.angle = angle
         self.height = height
+        self.upper_cut = upper_cut
         super().__init__()
 
     def _set_outer_radius(self) -> float:
@@ -23,7 +25,10 @@ class Cone(Figure):
         return [self.x0, self.y0, self.z0]
 
     def mu_params(self) -> List[float]:
-        return [self.angle, self.height]
+        if self.upper_cut:
+            return [self.angle, self.height, self.upper_cut]
+        else:
+            return [self.angle, self.height]
 
     def bounds_mut_params(self) -> List[List[float]]:
         if self.upper_cut:
@@ -38,12 +43,17 @@ class Cone(Figure):
         return (
             # sqrt(x^2 + y^2)*ctg(t)*sign(ctg(t))
             {'type': 'ineq', 'fun': lambda x: f(x)},
-            {'type': 'ineq', 'fun': lambda x: -1 * x[2] + self.z0},  # z < z0
+            {'type': 'ineq', 'fun': lambda x: -1 * x[2] + self.z0 - self.upper_cut},  # z < z0
             {'type': 'ineq', 'fun': lambda x: x[2] - (self.z0 - self.height)},  # z > z0 - h
         )
 
     def add_tol_to_mu_params(self, tolerances: List[float]):
         mu_params = self.mu_params()
         assert len(mu_params) == len(tolerances)
-        assert self.angle + tolerances[0] < np.pi / 2, 'It was' + str(self.angle) + ' and added' + str(tolerances[0])
-        return Cone(self.center, self.angle + tolerances[0], self.height + tolerances[1])
+        assert self.angle + tolerances[0] <= np.pi / 2, 'It was ' + str(self.angle) + ' and added' + str(tolerances[0])
+        if self.upper_cut:
+            assert 0 <= self.upper_cut - tolerances[2] <= self.upper_cut
+            return Cone(self.center, self.angle + tolerances[0], self.height + tolerances[1], self.upper_cut - tolerances[2])
+        else:
+            return Cone(self.center, self.angle + tolerances[0], self.height + tolerances[1])
+
